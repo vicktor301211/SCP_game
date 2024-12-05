27e63e844126f544eb1f9ae2a97dd07b4616fcbd
vicktor301211 <iva301211@gmail.com>
2024-12-05 18:26:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Foundation import *
+from Insurgency import *
+from SCPs import *
 from tkinter import *
 w = Tk()
 w.title('Меню')
 canv = Canvas(w, width=800, height=800, bg = 'alice blue')
 canv.pack()
+enemy = Insurgency(canvas=canv, x= 250, y= 250, hp=100, xp= 0, mentality_health=100, fraction=None, color='green')
 player = Foundation(canvas=canv, x = 350, y = 350, hp = 100, xp = 0, mentality_health=100, color = 'yellow')
 w.mainloop()
